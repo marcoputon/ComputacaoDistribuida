9ce63246ef5da7b7c23c9ac36a104dfcd98dca6c
marcoputon <marcoputon@gmail.com>
2017-07-24 10:40:05 -0300

    Correção de bug da troca de mensagens

--- TF_2.0/Server.py
+++ TF_2.0/Server.py
@@ -3,7 +3,17 @@ import requests, bottle, json, threading, time, sys, socket, pygame
 from Cycle import *
 from general import *
 
-my_ip = socket.gethostbyname(socket.gethostname())
+
+
+pygame.display.init()
+screen = pygame.display.set_mode((100, 100))
+
+
+
+
+
+#my_ip = socket.gethostbyname(socket.gethostname())
+my_ip = "172.20.84.110"
 
 try:
     port_ = int(sys.argv[1])
@@ -23,8 +33,9 @@ except:
     print("Please inform a valid list of peers.\n")
     exit(0)
 
-my_motor_cycle = Cycle() # Moto do server atual
-motor_cycles = {"http://" + my_ip + ":" + str(port_):my_motor_cycle} # Inicia com a moto do server atual
+my_id = "http://" + my_ip + ":" + str(port_)
+my_motor_cycle = Cycle((1, 2, 3)) # Moto do server atual
+motor_cycles = {my_id:my_motor_cycle} # Inicia com a moto do server atual
 
 
 @get('/peers')
@@ -61,7 +72,8 @@ def get_data():
                 data = json.loads(row_data.text)
                 peers_dict[peer] = True
                 for d in data:
-                    motor_cycles[d] = dict_to_cycle(data[d])
+                    if d != my_id:
+                        motor_cycles[d] = dict_to_cycle(data[d])
             except:
                 peers_dict[peer] = False
             time.sleep(0.5)
@@ -70,23 +82,41 @@ def get_data():
 # Tratar eventos
 def get_events():
     while True:
-        data = input()
-        motor_cycles.append(data)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
+
+                # Player keys
+                if event.key == pygame.K_DOWN:
+                    motor_cycles[my_id].path.append(["D", (0, 0, 0)])
+
+                if event.key == pygame.K_UP:
+                    motor_cycles[my_id].path.append(["U", (0, 0, 0)])
+
+                if event.key == pygame.K_LEFT:
+                    motor_cycles[my_id].path.append(["L", (0, 0, 0)])
+
+                if event.key == pygame.K_RIGHT:
+                    motor_cycles[my_id].path.append(["R", (0, 0, 0)])
 
 
 # Exibir localmente
 def show_data():
     while True:
+        '''
         print("motor_cycles - BEGIN")
         for i in motor_cycles:
-            print(i, motor_cycles[i])
+            print(i, motor_cycles[i].color)
         print("motor_cycles - END")
 
-        '''
         print("peers_dict")
         for i in peers_dict:
             print(i, peers_dict[i])
         '''
+        print(motor_cycles[my_id].path)
         time.sleep(1)
 
 
